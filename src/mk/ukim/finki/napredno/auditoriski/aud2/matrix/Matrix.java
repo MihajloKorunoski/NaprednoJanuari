634ffb84890f11a52c0d6be55c8e951fd69144a8
Mihaylo Markov Korunoski <mihajlo.korunoski@students.finki.ukim.mk>
2023-02-08 16:23:57 +0100

    first push

---
+++ src/mk/ukim/finki/napredno/auditoriski/aud2/matrix/Matrix.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.napredno.auditoriski.aud2.matrix;
+
+import java.util.Arrays;
+
+
+public class Matrix
+{
+    public static double sum(double[][] a){
+        return Arrays.stream(a).flatMapToDouble(Arrays::stream).sum();
+    }
+    public static double average(double[][] a){
+        return sum(a)/(a.length * a[0].length);
+    }
+
+    public static void main(String[] args)
+    {
+        double[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(Matrix.sum(a));
+        System.out.println(Matrix.average(a));
+    }
+}
---
+++ src/mk/ukim/finki/napredno/auditoriski/aud2/prefix/StringPrefix.java
@@ -0,0 +1,20 @@
+package mk.ukim.finki.napredno.auditoriski.aud2.prefix;
+
+import java.util.stream.IntStream;
+
+public class StringPrefix
+{
+    public static boolean isPrefix(String first, String second){
+        if(first.length() > second.length()){
+            return false;
+        }
+        return IntStream.range(0, first.length())
+                .allMatch(i -> first.charAt(i) == second.charAt(i));
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(StringPrefix.isPrefix("test", "testing"));
+        System.out.println(isPrefix("test", "apple"));
+        System.out.println(isPrefix("test", "tesla"));
+    }
+}
